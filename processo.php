a1cb2a6739908e2c09eea4dd1272d9cf292e16a1
gustavobentodm10 <gustavo.bento@dm10.com.br>
2022-10-24 11:09:15 -0400

    Segundo Commit

--- processo.php
+++ processo.php
@@ -7,15 +7,15 @@
      Mysql::conectar();
 
      if(isset($_POST['acao']) && $_POST['form'] == 'f_form'){
-             $nome = $_POST['Nome'];
-                $email = $_POST['Email'];
-                    $telefone = $_POST['Telefone'];
-                        $senha = $_POST['Senha'];
-                            $sexo = $_POST['Genero'];
-                                $nasc = $_POST['Nasc'];
-                                    $cidade = $_POST['Cidade'];
-                                        $estado = $_POST['Estado'];
-                                            $endereco = $_POST['Endereco'];
+            $nome = $_POST['Nome'];
+            $email = $_POST['Email'];
+            $telefone = $_POST['Telefone'];
+            $senha = $_POST['Senha'];
+            $sexo = $_POST['Genero'];
+            $nasc = $_POST['Nasc'];
+            $cidade = $_POST['Cidade'];
+            $estado = $_POST['Estado'];
+            $endereco = $_POST['Endereco'];
    
     Form::cadastrar($nome, $email, $telefone, $senha, $sexo, $nasc, $cidade, $estado, $endereco);
     echo "Usuario Cadastrado com Sucesso";
